c265ad45e71ab81d7b82f492e6ad73d6b6d7dbd1
Alistair MacDonald <amacdonald@aisquared.com>
2014-11-17 22:04:46 -0500

    Added circle function.

--- index.js
+++ index.js
@@ -10,6 +10,8 @@
     , cols = stdo.columns
     , rows = stdo.rows
     , defaultChar = ' '
+
+    , PI2 = Math.PI*2
     
     , color = {
         fg:{ r: 255, g: 255, b: 255 },
@@ -62,7 +64,7 @@
       get reset (){
         cursor.reset();
         cursor.goto(rows, cols);
-      }
+      },
     },
 
 
@@ -100,6 +102,18 @@
     },
 
 
+    circ: function (x, y, m) {
+      var res = m*PI2
+        , i
+        ;
+      
+      for(i=0; i< res; i+=1){
+        var loc = PI2/res * i;
+        this.point(x+Math.sin(loc)*m,y+Math.cos(loc)*m/2);
+      }
+    },
+
+
     // Get the distance between two points
     dist: function (x1, y1, x2, y2){
       return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
@@ -125,9 +139,18 @@
       // if 
     },
 
+    text: function(x, y, text){
+      cursor.goto(x,y).write(text);
+    },
+
+
+    // moveTo: function (x, y) {
+    //   cursor.moveTo()
+    // },
+
+
     // Changes foreground color
     fg: function (r, g, b) {
-      color
       cursor.fg.rgb(r,g,b);
     },
 
